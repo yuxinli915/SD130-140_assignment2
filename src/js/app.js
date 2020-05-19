dc6ccfc44ece7032cf131fbdcc174bfd9fcede13
Yuxin Li <yuxinli915@outlook.com>
2020-05-19 17:56:51 -0500

    Get current conditions from API.

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,19 @@
-const apiKey = `6948fd5a50d6bf51eb00c914a734fd74`
+const apiKey = `6948fd5a50d6bf51eb00c914a734fd74`;
 
-const getLocation = new Promise((resolve, reject) => {
-  navigator.geolocation.getCurrentPosition(
-    position => { resolve(position.coords) },
-    error => { reject(error) }
-  );
-}).then(coordinates => {
-  getCurrentConditions(coordinates.latitude, coordinates.longitude);
-}).catch(error => console.log(`Fail to locate!`));
+navigator.geolocation.getCurrentPosition(position => {
+  getCurrentConditions(position.coords.latitude, position.coords.longitude);
+});
 
-
-
-// fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${apiKey}`)
-//   .then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     } else {
-//       throw new Error(`Fail to retrieve data.`);
-//     }
-//   })
-//   .then(data => {
-//     console.log(data);
-//   })
+function getCurrentConditions(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Fail to get user location.`);
+      }
+    })
+    .then(data => {
+      console.log(data);
+    })
+}
